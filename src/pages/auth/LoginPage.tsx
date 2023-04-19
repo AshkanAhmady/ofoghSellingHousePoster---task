e1078c7011fc9b8ceaebbba28fac9356877d71fd
ashkan <ashkan16ma96@gmail.com>
2023-04-20 01:33:52 +0430

    login&register ui page created

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,5 +1,58 @@
+import { useState } from "react";
+
 const LoginPage = () => {
-    return ( <div>Login</div> );
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const changeHandler = (e: React.FormEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [e.currentTarget.name]: e.currentTarget.value })
+    }
+
+    const submitHandler = (e: React.FormEvent<HTMLInputElement>) => {
+        e.preventDefault()
+        console.log("form data =>", formData)
+    }
+
+    return (<div className="mt-6 flex justify-center">
+        <form className="bg-white rounded-xl shadow-lg flex flex-col w-full md:w-[40%] p-3">
+            <h1 className="text-2xl text-center">ورود</h1>
+            <hr className="my-4" />
+            <label className="text-stone-500 font-medium" htmlFor="email">
+                ایمیل
+            </label>
+            <input
+                placeholder="ایمیل خود را وارد کنید..."
+                className="bg-transparent placeholder:text-stone-400 outline-none border border-stone-300 rounded px-2 py-1 mb-5 mt-1"
+                onChange={(e) => changeHandler(e)}
+                name="email"
+                type="email"
+                value={formData.email}
+                id="email"
+            />
+            <label className="text-stone-500 font-medium" htmlFor="password">
+                رمز عبور
+            </label>
+            <input
+                placeholder="رمز خود را وارد کنید..."
+                className="bg-transparent placeholder:text-stone-400 outline-none border border-stone-300 rounded px-2 py-1 mb-5 mt-1"
+                onChange={(e) => changeHandler(e)}
+                name="password"
+                type="password"
+                value={formData.password}
+                id="password"
+            />
+            <div className="w-full flex gap-4">
+                <input
+                    onClick={submitHandler}
+                    className="bg-blue-600 cursor-pointer flex-auto text-white px-7 py-1 rounded-md"
+                    type="submit"
+                    value="ورود"
+                />
+            </div>
+        </form>
+    </div>);
 }
- 
+
 export default LoginPage;
--- src/pages/auth/RegisterPage.tsx
+++ src/pages/auth/RegisterPage.tsx
@@ -1,5 +1,71 @@
+import { useState } from "react";
+
 const RegisterPage = () => {
-    return ( <div>register page</div> );
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const changeHandler = (e: React.FormEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [e.currentTarget.name]: e.currentTarget.value })
+    }
+
+    const submitHandler = (e: React.FormEvent<HTMLInputElement>) => {
+        e.preventDefault()
+        console.log("form data =>", formData)
+    }
+
+    return (<div className="mt-6 flex justify-center">
+        <form className="bg-white rounded-xl shadow-lg flex flex-col w-full md:w-[40%] p-3">
+            <h1 className="text-2xl text-center">ثبت نام</h1>
+            <hr className="my-4" />
+            <label className="text-stone-500 font-medium" htmlFor="email">
+                ایمیل
+            </label>
+            <input
+                placeholder="ایمیل خود را وارد کنید..."
+                className="bg-transparent placeholder:text-stone-400 outline-none border border-stone-300 rounded px-2 py-1 mb-5 mt-1"
+                onChange={(e) => changeHandler(e)}
+                name="email"
+                type="email"
+                value={formData.email}
+                id="email"
+            />
+            <label className="text-stone-500 font-medium" htmlFor="password">
+                رمز عبور
+            </label>
+            <input
+                placeholder="رمز خود را وارد کنید..."
+                className="bg-transparent placeholder:text-stone-400 outline-none border border-stone-300 rounded px-2 py-1 mb-5 mt-1"
+                onChange={(e) => changeHandler(e)}
+                name="password"
+                type="password"
+                value={formData.password}
+                id="password"
+            />
+            <label className="text-stone-500 font-medium" htmlFor="confirmPassword">
+                تکرار رمز عبور
+            </label>
+            <input
+                placeholder="رمز را دوباره وارد کنید..."
+                className="bg-transparent placeholder:text-stone-400 outline-none border border-stone-300 rounded px-2 py-1 mb-5 mt-1"
+                onChange={(e) => changeHandler(e)}
+                name="confirmPassword"
+                type="password"
+                value={formData.confirmPassword}
+                id="confirmPassword"
+            />
+            <div className="w-full flex gap-4">
+                <input
+                    onClick={submitHandler}
+                    className="bg-blue-600 cursor-pointer flex-auto text-white px-7 py-1 rounded-md"
+                    type="submit"
+                    value="ثبت نام"
+                />
+            </div>
+        </form>
+    </div>);
 }
- 
+
 export default RegisterPage;
